f9e11b819f48ffee877ff8c8f38a2374235e3056
Anastasiia Mishunina <anastasiiamishunina@MacBook-Pro-Anastasiia.local>
2020-03-21 19:53:22 +0300

    init project

---
+++ src/components/TileMap/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as styles from './TileMap.scss'
+
+export default props => {
+    return (
+        <div className={styles['tile-map']}>
+            <div className={styles['tile-map__content']}>
+                Hello! I am future tile map. Please, implement me =)
+            </div>
+        </div>
+    )
+}
