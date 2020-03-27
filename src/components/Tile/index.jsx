6695451a90aedc4ed36e84e36e087a31cccc1971
JiangBey <session_dynasty@mail.ru>
2020-03-27 10:21:01 +0300

    tile

---
+++ src/components/Tile/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as styles from './TileMap.scss';
+
+export default () => (
+  <div className={styles['tile-map']}>
+    <div className={styles['tile-map__content']}>
+      Hello! I am future tile map. Please, implement me =)
+    </div>
+  </div>
+);
