bbfe7d49543e93b8b9ee36f41297a15c897adac7
JiangBey <session_dynasty@mail.ru>
2020-04-04 21:09:54 +0300

    added color

--- src/app.jsx
+++ src/app.jsx
@@ -4,16 +4,10 @@ import TileMap from 'Components/TileMap';
 import style from './app.css';
 import data from './data/data.js';
 
-
 const App = () => (
-    <div className={style.app}>
-        <TileMap
-            data={data}
-            mapHeight={300}
-            mapWidth={600}/>
-    </div>
+  <div className={style.app}>
+    <TileMap data={data} mapHeight={500} mapWidth={1000} />
+  </div>
 );
 
 export default App;
-
-
--- src/components/TileMap/index.jsx
+++ src/components/TileMap/index.jsx
@@ -1,54 +1,47 @@
 import React, { useEffect, useState } from 'react';
-import * as styles from './TileMap.scss';
 import * as d3 from 'd3';
-
+import * as styles from './TileMap.scss';
 
 export default ({ mapHeight, mapWidth, data }) => {
+  useEffect(() => {
+    addTileMapContent(data);
+  }, []);
 
+  const addTileMapContent = data => {
+    const svg = d3.select('#mapContent').append('svg');
 
-    useEffect(() => {
-        addTileMapContent(data)
-    }, []);
+    svg.attr('width', mapWidth).attr('height', mapHeight);
 
-    const addTileMapContent = data => {
+    const maxColumns = d3.max(data, d => parseInt(d.col));
+    const maxRows = d3.max(data, d => parseInt(d.row));
 
-        const svg = d3.select('#mapContent').append('svg');
+    const tileWidth = mapWidth / (maxColumns + 1);
+    const tileHeight = mapHeight / (maxRows + 1);
 
-        svg.attr('width', mapWidth)
-            .attr('height', mapHeight);
+    svg.append('g').attr('id', 'tileArea');
 
-        const maxColumns = d3.max(data, d => parseInt(d.col));
-        const maxRows = d3.max(data, d => parseInt(d.row));
+    const tile = svg
+      .select('#tileArea')
+      .selectAll('g')
+      .data(data)
+      .enter()
+      .append('g');
 
-        const tileWidth = mapWidth / (maxColumns + 1);
-        const tileHeight = mapHeight / (maxRows + 1);
+    tile
+      .append('rect')
+      .style('fill', '#8aebf6')
+      .attr('width', tileWidth)
+      .attr('height', tileHeight)
+      .attr('x', d => d.col * tileWidth)
+      .attr('y', d => d.row * tileHeight)
+      .classed(styles['tile-map__tile'], true);
 
-        svg.append('g').attr('id', 'tileArea');
+    return svg;
+  };
 
-        const tile = svg
-            .select('#tileArea')
-            .selectAll('g')
-            .data(data)
-            .enter()
-            .append('g');
-
-        tile.append('rect')
-            .attr('width', tileWidth)
-            .attr('height', tileHeight)
-            .attr('x', d => d.col * tileWidth)
-            .attr('y', d => d.row * tileHeight)
-            .classed(styles['tile-map__tile'], true);
-
-        return svg;
-    };
-
-    return (
-        <div className={styles['tile-map']}>
-            <div className={styles['tile-map__content']} id={'mapContent'}>
-            </div>
-        </div>
-    )
-}
-
-
-
+  return (
+    <div className={styles['tile-map']}>
+      <div className={styles['tile-map__content']} id="mapContent" />
+    </div>
+  );
+};
